4beb992b6b6726473bb62bfe562026fb3b394dcc
Valerie <valeriedeleon4521@gmail.com>
2019-10-22 08:53:53 -0700

    Create wordsActivity.java

---
+++ wordsActivity.java
@@ -0,0 +1,14 @@
+package com.example.puzzlewise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class wordsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_words);
+    }
+}
