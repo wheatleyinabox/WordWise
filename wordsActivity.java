c9d25463c0e9b1805ef6e1a78f578a8b2b60feb7
Valerie <valeriedeleon4521@gmail.com>
2019-10-24 08:57:32 -0700

    Update wordsActivity.java

--- wordsActivity.java
+++ wordsActivity.java
@@ -10,31 +10,44 @@ import android.widget.TextView;
 
 public class wordsActivity extends AppCompatActivity {
 
-    private EditText wordsInput;
-    //private String[] list = new String[5];
-    private TextView wordList;
-    private Button addBtn;
-    private String word;
+    String[] list = new String[5];
+    TextView wordList;
+    Button addBtn;
+    String word1, word2, word3, word4, word5;
+    EditText input1, input2, input3, input4, input5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_words);
 
-        wordsInput = (EditText) findViewById(R.id.input);
-
         wordList = (TextView) findViewById(R.id.wordList);
 
+        input1 = (EditText) findViewById(R.id.input1);
+        input2 = (EditText) findViewById(R.id.input2);
+        input3 = (EditText) findViewById(R.id.input3);
+        input4 = (EditText) findViewById(R.id.input4);
+        input5 = (EditText) findViewById(R.id.input5);
+
         addBtn = (Button) findViewById(R.id.addBtn);
-        addBtn.setOnClickListener(new View.OnClickListener()
-        {
+        addBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v)
             {
-                wordList.setText(word);
+                word1 = input1.getText().toString();
+                word2 = input2.getText().toString();
+                word3 = input3.getText().toString();
+                word4 = input4.getText().toString();
+                word5 = input5.getText().toString();
+                list[0] = word1;
+                list[1] = word2;
+                list[2] = word3;
+                list[3] = word4;
+                list[4] = word5;
+
+                wordList.setText(list[0] + "\n" + list[1] + "\n" + list[2] + "\n" + list[3] + "\n" + list[4]);
             }
         });
 
-        word = wordsInput.getText().toString();
     }
 
 
