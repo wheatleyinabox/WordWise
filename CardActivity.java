b88a4c3b7ab6d20213b370bdbcae9a6a9b560326
Valerie <valeriedeleon4521@gmail.com>
2019-10-31 18:23:20 -0700

    Update CardActivity.java

--- CardActivity.java
+++ CardActivity.java
@@ -2,14 +2,14 @@ package com.example.puzzlewise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class CardActivity extends AppCompatActivity {
 
-    Button card, resetBtn;
-    Boolean reset = false;
+    Button card, reset, home;
     int count = 0;
 
     @Override
@@ -21,23 +21,45 @@ public class CardActivity extends AppCompatActivity {
         final String[] cardList = extras.getStringArray("cardList");
 
         card = findViewById(R.id.card);
+        card.setText("Click Me!");
 
         card.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
-                while (reset == false)
-                {
-                    card.setText(cardList[count]);
-                    count++;
-                    if (count == cardList.length)
-                    {
-                        count = 0;
-                    }
+            public void onClick(View view) {
+                card.setText(cardList[count]);
+                count++;
+                if (count == cardList.length) {
+                    count = 0;
                 }
             }
         });
 
-        //resetBtn = findViewById(R.id.resetButton);
+        home = findViewById(R.id.home);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
+
+        reset = findViewById(R.id.reset);
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWordsActivity();
+            }
+        });
+    }
+
+    public void openMainActivity()
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void openWordsActivity()
+    {
+        Intent intent = new Intent(this, wordsActivity.class);
+        startActivity(intent);
     }
 }
